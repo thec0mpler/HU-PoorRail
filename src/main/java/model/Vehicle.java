d01f347239f0cfab3fb5ab6989ae70a0d7ba9e38
aversio <ua_gs_70@hotmail.com>
2017-02-06 22:32:03 +0100

    Added (missed) abstract methods in Vehicle

--- src/main/java/model/Vehicle.java
+++ src/main/java/model/Vehicle.java
@@ -10,6 +10,14 @@ public abstract class Vehicle {
         return this.seats;
     }
 
+    public abstract String getName();
+
+    public abstract void setName(String name);
+
+    public abstract boolean equals(Object object);
+
+    public abstract String toString();
+
     public void setSeats(int seats) {
         this.seats = seats;
     }
