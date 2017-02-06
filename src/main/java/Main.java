7eff50b925004f518ba11083bf454f7d627b504e
Jonathan Peeman <contact@jonathanpeeman.nl>
2017-02-06 23:22:27 +0100

    Merge 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,39 +3,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import model.Commands;
-import model.Train;
-import model.Vehicle;
 
-import java.util.Scanner;
-
-public class Main/* extends Application */{
+public class Main extends Application {
     public static void main(String[] args) {
-        Commands addCommand = Commands.ADD;
-        Scanner input = new Scanner(System.in);
-        String i = input.next().toUpperCase();    //String input = "add 3 to 5"; // We moeten ID gebruiken ipv variable namen
-
-        //i.toUpperCase(); //miss niet meer nodig door 1 regel hierboven.
-
-        if (eersteWord.equals(addCommand.name())) {  // name() maakt t String
-            if (trainManager.zoekTrein(tweedeWord)) {
-                Train train = trainManager.getTrainByID(tweedeWord);
-                if (derdeWord.equals("TO")) {
-                    train.add(vierdeWord);
-                }
-            } else {
-                // error
-            }
-        }
-        else {
-            // error
-        }
+        launch(args);
     }
-    Train t = new Train("d");
-    private String execute(String input) {
-        return "The command nr. 1 is executed.";
-    }
-   /* @Override
+
+    @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("RichRail.fxml"));
 
@@ -44,10 +18,5 @@ public class Main/* extends Application */{
         primaryStage.setResizable(false);
 
         primaryStage.show();
-    }*/
-//   Train t = new Train("hmm");
-//    t.addVehicle
-
-
-
+    }
 }
